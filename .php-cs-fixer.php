f80ddf7eb6b293660f80a299580e8082a2f523b9
Rifki Alhuraibi <veelasky@pm.me>
2022-01-26 22:48:18 +0700

    init package

---
+++ .php-cs-fixer.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PSR2' => true,
+        '@PSR12' => true,
+        '@PSR1' => true,
+        '@Symfony' => true,
+        'array_syntax' => true,
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'multiline_whitespace_before_semicolons' => ['strategy'=>'no_multi_line'],
+        'no_alias_functions' => true,
+        'no_superfluous_phpdoc_tags' => false,
+        'not_operator_with_successor_space' => true,
+        'ordered_imports' => ['sort_algorithm'=>'length'],
+        'phpdoc_no_empty_return' => false,
+        'self_accessor' => true,
+        'simplified_null_return' => true,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->in(__DIR__)
+    );
---
+++ README.md
@@ -0,0 +1,3 @@
+## Laravel Concerns
+--
+WIP
